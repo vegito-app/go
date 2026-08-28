1d93208db4ecdd91dd7e1f537f6198c31770f866
David Berichon <david.berichon@vegito.app>
2026-08-28 12:35:56 +0000

    feat(metrics): ajouter un service de métriques avec gestionnaire HTTP

---
+++ prometheus/http.go
@@ -0,0 +1,26 @@
+package prometheus
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type MetricsService struct {
+	metrics Metrics
+}
+
+type Metrics interface {
+	prometheus.Gatherer
+}
+
+func NewMetricsService(mux *http.ServeMux, metricsProvider Metrics) (*MetricsService, error) {
+	service := &MetricsService{
+		metrics: metricsProvider,
+	}
+
+	mux.Handle("/metrics", promhttp.HandlerFor(metricsProvider, promhttp.HandlerOpts{}))
+
+	return service, nil
+}
